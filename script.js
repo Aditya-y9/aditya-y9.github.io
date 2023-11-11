107c6e6c39de5cadbcc483494ba47305c86e50dc
Aditya-y9 <aditya.yedurkar@gmail.com>
2023-11-11 23:38:16 +0530

    Blob and Blur added

--- script.js
+++ script.js
@@ -68,3 +68,14 @@ var tl2 = gsap.timeline({
 tl2.to(".main",{
     backgroundColor:"#fff"
 })
+
+
+const blob = document.getElementById("blob");
+window.onpointermove = event => {
+    const {clientX, clientY} = event;
+
+    blob.animate({
+        left : `${clientX}px`,
+        top: `${clientY}px`
+    },{duration: 1000, fill: "forwards"})
+}
